f58359180b9e1283d2f3f0874157814367248032
josstei <48696594+josstei@users.noreply.github.com>
2026-01-31 22:44:16 -0500

    chore: simplify colors entry point and remove old files

--- colors/voidpulse.lua
+++ colors/voidpulse.lua
@@ -1,8 +1,1 @@
-if vim.g.colors_name then
-    vim.cmd("highlight clear")
-end
-
-vim.g.colors_name   = "voidpulse"
-vim.o.termguicolors = true
-
-require("voidpulse").setup()
+require("voidpulse").load()
